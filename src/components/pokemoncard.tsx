08ae66872b38f8507e10d5aad83446f08fb5604f
KADRI JUGURTA <jugurtha.kadri@gmail.com>
2025-10-22 08:45:14 +0200

    creationcomposantdynamique

--- src/components/pokemoncard.tsx
+++ src/components/pokemoncard.tsx
@@ -1,21 +1,30 @@
 import "./pokemoncard.css";
 
 const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-	imgSrc:
-  },
+	{
+		name: "bulbasaur",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+	{
+		name: "mew",
+		imgSrc: undefined,
+	},
 ];
 
 function PokemonCard() {
-	const pokemon = pokemonList[0];
+	const pokemon = pokemonList[0]; // change l’index pour tester
 
+	return (
+		<figure className="pokemon-card">
+			{pokemon.imgSrc ? (
+				<img src={pokemon.imgSrc} alt={pokemon.name} />
+			) : (
+				<p>???</p>
+			)}
+			<figcaption>{pokemon.name}</figcaption>
+		</figure>
+	);
 }
-
 
 export default PokemonCard;
