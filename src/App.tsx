c48b7541c9e853b8a1a52aba1195a3527504f6cd
KADRI JUGURTA <jugurtha.kadri@gmail.com>
2025-10-27 10:09:21 +0100

    Ajout du dossier component.2 sans sous-module + modifs App.tsx, pokemoncard.tsx, tsconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import "./App.css";
 import PokemonCard from "./components/pokemoncard";
 
+const pokemonList = [
+	{
+		name: "bulbasaur",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+	{
+		name: "mew",
+		imgSrc: undefined,
+	},
+];
+
 function App() {
-	return <PokemonCard />;
+	const pokemon = pokemonList[0];
+
+	return <PokemonCard pokemon={pokemon} />;
 }
 
 export default App;
--- src/components/pokemoncard.tsx
+++ src/components/pokemoncard.tsx
@@ -1,21 +1,14 @@
-import "./pokemoncard.css";
-
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-	imgSrc:
-  },
-];
-
-function PokemonCard() {
-	const pokemon = pokemonList[0];
-
+function PokemonCard({ pokemon }) {
+	return (
+		<figure className="pokemon-card">
+			{pokemon.imgSrc ? (
+				<img src={pokemon.imgSrc} alt={pokemon.name} />
+			) : (
+				<p>Image indisponible</p>
+			)}
+			<figcaption>{pokemon.name}</figcaption>
+		</figure>
+	);
 }
-
 
 export default PokemonCard;
